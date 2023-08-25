458e31e5d0e603f1c5accc4d8d7a4499ce30742b
Raphael <raphael.nascimento@pxbank.com.br>
2023-08-25 10:43:14 -0300

    Primeira refatoração

--- app/src/main/java/com/example/listadeagentesvalorant/MainActivity.kt
+++ app/src/main/java/com/example/listadeagentesvalorant/MainActivity.kt
@@ -8,33 +8,83 @@ import com.example.listadeagentesvalorant.Adapter.AdapterDuelistas
 import com.example.listadeagentesvalorant.model.Agente
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recyclerView_agentesDuelistas : RecyclerView
+    lateinit var recyclerView_agentesSentinelas : RecyclerView
+    lateinit var recyclerView_agentesControladores : RecyclerView
+    lateinit var recyclerView_agentesIniciadores : RecyclerView
+    lateinit var duelistasValorant : MutableList<Agente>
+    lateinit var sentinelasValorant : MutableList<Agente>
+    lateinit var controladoresValorant : MutableList<Agente>
+    lateinit var iniciadoresValorant : MutableList<Agente>
+    lateinit var jett : Agente
+    lateinit var raze : Agente
+    lateinit var neon : Agente
+    lateinit var yoru : Agente
+    lateinit var reyna : Agente
+    lateinit var killjoy : Agente
+    lateinit var cypher : Agente
+    lateinit var deadlock : Agente
+    lateinit var sage : Agente
+    lateinit var chamber : Agente
+    lateinit var brim : Agente
+    lateinit var astra : Agente
+    lateinit var omen : Agente
+    lateinit var viper : Agente
+    lateinit var skye : Agente
+    lateinit var kayo : Agente
+    lateinit var breach : Agente
+    lateinit var gekko : Agente
+    lateinit var sova : Agente
+    lateinit var fade : Agente
+    lateinit var harbor : Agente
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView_agentesDuelistas = findViewById<RecyclerView>(R.id.agentes_duelistas)
-        val recyclerView_agentesSentinelas = findViewById<RecyclerView>(R.id.agentes_sentinelas)
-        val recyclerView_agentesControladores = findViewById<RecyclerView>(R.id.agentes_controladores)
-        var recyclerView_agentesIniciadores = findViewById<RecyclerView>(R.id.agentes_iniciadores)
+        pegaids()
+        setLayout()
+        criaListas()
+        configuraAdapter()
+        criaDuelistas()
+        criaControladores()
+        criaIniciadores()
+        criaSentinelas()
+        adicionaNaLista()
 
+    }
+
+    fun pegaids(){
+        recyclerView_agentesDuelistas = findViewById(R.id.agentes_duelistas)
+        recyclerView_agentesSentinelas = findViewById(R.id.agentes_sentinelas)
+        recyclerView_agentesControladores = findViewById(R.id.agentes_controladores)
+        recyclerView_agentesIniciadores = findViewById(R.id.agentes_iniciadores)
+    }
+
+    fun setLayout() {
         //define a direção da lista
         recyclerView_agentesControladores.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
         recyclerView_agentesDuelistas.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerView_agentesSentinelas.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         recyclerView_agentesIniciadores.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false)
 
-
         //melhora o desempenho da lista
         recyclerView_agentesDuelistas.setHasFixedSize(true)
         recyclerView_agentesSentinelas.setHasFixedSize(true)
         recyclerView_agentesControladores.setHasFixedSize(true)
         recyclerView_agentesIniciadores.setHasFixedSize(true)
+    }
 
-        val duelistasValorant : MutableList<Agente> = mutableListOf()
-        val sentinelasValorant : MutableList<Agente> = mutableListOf()
-        val controladoresValorant : MutableList<Agente> = mutableListOf()
-        val iniciadoresValorant : MutableList<Agente> = mutableListOf()
+    fun criaListas(){
+        duelistasValorant = mutableListOf()
+        sentinelasValorant = mutableListOf()
+        controladoresValorant = mutableListOf()
+        iniciadoresValorant = mutableListOf()
+    }
 
+    fun configuraAdapter(){
         val adapterDuel = AdapterDuelistas(this, duelistasValorant)
         val adapterSenti = AdapterDuelistas(this, sentinelasValorant)
         val adapterControl = AdapterDuelistas(this, controladoresValorant)
@@ -44,113 +94,122 @@ class MainActivity : AppCompatActivity() {
         recyclerView_agentesSentinelas.adapter = adapterSenti
         recyclerView_agentesControladores.adapter = adapterControl
         recyclerView_agentesIniciadores.adapter = adapterInits
+    }
 
-        val jett = Agente(
+    fun criaDuelistas(){
+        jett = Agente(
             R.drawable.jett,
             "Jett"
         )
 
-        val reyna = Agente(
+        reyna = Agente(
             R.drawable.reyna,
             "Reyna"
         )
 
-        val yoru = Agente(
+        yoru = Agente(
             R.drawable.yoru,
             "Yoru"
         )
 
-        val raze = Agente(
+        raze = Agente(
             R.drawable.raze,
             "Raze"
         )
 
-        val neon = Agente(
+        neon = Agente(
             R.drawable.neon,
             "Neon"
         )
+    }
 
-        val killjoy = Agente(
+    fun criaSentinelas(){
+        killjoy = Agente(
             R.drawable.killjoy,
             "Killjoy"
         )
 
-        val cypher = Agente(
+        cypher = Agente(
             R.drawable.cypher,
             "Cypher"
         )
 
-        val deadlock = Agente(
+        deadlock = Agente(
             R.drawable.deadlock,
             "Deadlock"
         )
 
-        val chamber = Agente(
+        chamber = Agente(
             R.drawable.chamber,
             "Chamber"
         )
 
-        val sage = Agente(
+        sage = Agente(
             R.drawable.sage,
             "Sage"
         )
+    }
 
-        val brim = Agente(
+    fun criaControladores(){
+        brim = Agente(
             R.drawable.brim,
             "Brimstone"
         )
 
-        val viper = Agente(
+        viper = Agente(
             R.drawable.viper,
             "Viper"
         )
 
-        val omen = Agente(
+        omen = Agente(
             R.drawable.omen,
             "Omen"
         )
 
-        val astra = Agente(
+        astra = Agente(
             R.drawable.astra,
             "Astra"
         )
 
-        val harbor = Agente(
+        harbor = Agente(
             R.drawable.harbor,
             "Harbor"
         )
+    }
 
-        val skye = Agente(
+    fun criaIniciadores(){
+        skye = Agente(
             R.drawable.skye,
             "Skye"
         )
 
-        val breach = Agente(
+        breach = Agente(
             R.drawable.breach,
             "Breach"
         )
 
-        val fade = Agente(
+        fade = Agente(
             R.drawable.fade,
             "Fade"
         )
 
-        val kayo = Agente(
+        kayo = Agente(
             R.drawable.kayo,
             "Kayo"
         )
 
-        val sova = Agente(
+        sova = Agente(
             R.drawable.sova,
             "Sova"
         )
 
-        val gekko = Agente(
+        gekko = Agente(
             R.drawable.gekko,
             "Gekko"
         )
+    }
 
-
+    fun adicionaNaLista(){
         duelistasValorant.add(jett)
         duelistasValorant.add(reyna)
         duelistasValorant.add(yoru)
